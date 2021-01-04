33f1772d40516557fa7344dcb8296ae63ce0fffc
Kayla Newlon <kbnewlon@hotmail.com>
2021-01-03 19:55:54 -0800

    added media card component

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import React from 'react';
 import './App.css';
 import '@shopify/polaris/dist/styles.css';
 import enTranslations from '@shopify/polaris/locales/en.json';
-import {AppProvider, Page, TopBar, Frame} from '@shopify/polaris';
-import SearchForm from './component/SearchForm'
+import { AppProvider, Page, TopBar} from '@shopify/polaris';
+import SearchForm from './component/SearchForm';
+import MediaCard from './component/MediaCard'
 
 
-function App(){
-  return(
-  <AppProvider i18n={enTranslations}>
-  
-    <TopBar />
-    <SearchForm />
-    <Page title="Reasons I want to work at Shopify"><p>An Interactive Experience by Kayla Newlon</p></Page>
+function App() {
+  return (
+    <AppProvider i18n={enTranslations}>
 
-  </AppProvider>
-  )};
+      <TopBar />
+      <SearchForm />
+      <Page title="Reasons I want to work at Shopify"><p>An Interactive Experience by Kayla Newlon</p></Page>
+      <MediaCard />
+    </AppProvider>
+  )
+};
 
 export default App;
